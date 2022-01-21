cdb8c38ff6d61e217c00c0af510ad189ae78840c
DabooUA <sbardysh@tutanota.com>
2022-01-21 11:20:08 -0500

    adding textarea to NewCommentForm component

--- src/components/NewCommentForm.js
+++ src/components/NewCommentForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { updateNewCommentForm } from '../actions/newCommentForm.js'
 import { connect } from "react-redux";
+import Home from "./Home.js";
 
 const NewCommentForm = ({ updateNewCommentForm, formData }) => {
   const { content } = formData
@@ -16,7 +17,10 @@ const NewCommentForm = ({ updateNewCommentForm, formData }) => {
   }
   return(
     <form onSubmit={handleSubmit}>
-      <br/><br/><input placeholder="Type your comment" name="content" onChange={handleChange} value={content} /><br/><br/>
+      <label>
+      Comment: 
+      <br/><br/><textarea placeholder="Type your comment" name="content" onChange={handleChange} value={content} /><br/><br/>
+      </label>
       <input type="submit" value="Post Comment"/>
     </form>
   )

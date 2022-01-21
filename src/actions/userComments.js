c842b0f145e85a40add29178d6121d1d745af971
DabooUA <sbardysh@tutanota.com>
2022-01-21 11:18:49 -0500

    adding getComments and createComment to userComments action

--- src/actions/userComments.js
+++ src/actions/userComments.js
@@ -32,3 +32,55 @@ export const updateUserComment = comment => {
     comment
   }
 }
+
+export const getUserComments = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/comments", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setUserComment(response.data))
+        }
+      })
+      .catch(console.log)
+  }
+}
+
+export const createUserComment = (commentData, history) => {
+  return dispatch => {
+    const sendCommentData = {
+      content: commentData.content,
+      user_id: commentData.user_id,
+      repairshop_id: commentData.repairshop_id
+    }
+
+    return fetch("http://localhost:3001/api/v1/comments", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sendCommentData)
+    })
+    .then(r => r.json())
+      .then(resp => {
+        if (resp.error) {
+          alert(resp.error)
+        } else {
+          dispatch(addComment(resp.data))
+          dispatch(resetCommentForm())
+          history.push(`/comments/${resp.data.id}`)
+        }
+      })
+      .catch(console.log)
+  }
+}
+
